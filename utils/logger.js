1218f4746cdcaeb71b5cd54db66205f71b60232e
shadyyd <yd.radwan@gmail.com>
2024-08-30 20:28:17 +0300

    vercel editing files 2

--- utils/logger.js
+++ utils/logger.js
@@ -1,8 +1,13 @@
 const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, printf, colorize } = format;
-
+const fs = require("fs");
 const path = require("path");
-const filpath = path.join(__dirname, "./../logs/");
+
+const logDir = path.join(__dirname, "../logs");
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
 const logFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`;
@@ -20,8 +25,11 @@ const logger = createLogger({
     new transports.Console({
       format: combine(colorize(), logFormat),
     }),
-    new transports.File({ filename: `${filpath}error.log`, level: "error" }),
-    new transports.File({ filename: `${filpath}combined.log` }),
+    new transports.File({
+      filename: path.join(logDir, "error.log"),
+      level: "error",
+    }),
+    new transports.File({ filename: path.join(logDir, "combined.log") }),
   ],
 });
 
